e876951d7f72619eae093ab2cfc67ea628349979
Erinna Chen <erinna.chen@gmail.com>
2016-02-21 12:30:05 -0700

    Added factory girl to test out song

---
+++ spec/support/factories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :artist do
+    name "Taylor Swift"
+    image_path "https://upload.wikimedia.org/wikipedia/commons/b/b2/Taylor_Swift_Red_Tour_2%2C_2013.jpg"
+  end
+end
