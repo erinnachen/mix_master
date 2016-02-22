2b894b954f3ad6efcfa184002b7e170ff7dc7051
Erinna Chen <erinna.chen@gmail.com>
2016-02-21 22:13:25 -0700

    Iteration 6: Added invalid params tests to PlaylistsControllers#create

--- spec/controllers/playlists_controller_spec.rb
+++ spec/controllers/playlists_controller_spec.rb
@@ -2,26 +2,39 @@ require 'rails_helper'
 
 RSpec.describe PlaylistsController, type: :controller do
   describe "POST #create" do
-    context "create with valid params" do
+    context "with valid params" do
       it "creates a new playlist" do
-        playlist = create(:playlist)
         expect {
           post :create, {:playlist => attributes_for(:playlist)}
         }.to change(Playlist, :count).by(1)
       end
 
       it "assigns the new playlist to @playlist" do
-        playlist = create(:playlist)
         post :create, {:playlist => attributes_for(:playlist)}
         expect(assigns(:playlist)).to be_a(Playlist)
         expect(assigns(:playlist)).to be_persisted
       end
 
       it "redirects to the show playlist path" do
-        playlist = create(:playlist)
         post :create, {:playlist => attributes_for(:playlist)}
         expect(response).to redirect_to playlist_path(Playlist.last)
       end
     end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved playlist as @playlist" do
+        expect {
+        post :create, {:playlist => attributes_for(:playlist, name: nil)}
+      }.not_to change(Playlist, :count)
+        expect(assigns(:playlist)).to be_a_new(Playlist)
+      end
+
+      it "re-renders the template new" do
+        post :create, {:playlist => attributes_for(:playlist, name: nil)}
+        expect(response).to render_template "new"
+      end
+    end
+
+
   end
 end
